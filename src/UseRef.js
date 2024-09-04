4dd7fb0a870a5b57a3d1f54cf139e86873a743c0
anjali-2911 <96237256+anjali-2911@users.noreply.github.com>
2024-09-05 00:05:00 +0530

    usehooks commit

---
+++ src/UseRef.js
@@ -0,0 +1,19 @@
+import React, {useState, useRef, useEffect} from 'react'
+
+const UseRef = () => {
+    const [name, setName] = useState(0);
+    const count = useRef(0);
+
+    useEffect(()=>{
+        count.current = count.current+1;
+    })
+  return (
+    <div>
+        <input type='text' onChange={(e)=>setName(e.target.value)}/>
+      <h1>name : {name}</h1>
+      <h1>Count : {count.current}</h1>
+    </div>
+  )
+}
+
+export default UseRef
